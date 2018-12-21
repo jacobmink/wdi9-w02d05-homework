ee09da1afbb944368de124c2b2feb24440a385d6
Jacob Mink <jacobmink@gmail.com>
2018-12-21 16:21:03 -0700

    Chapter 2 complete - Made the Hobbits

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -76,13 +76,16 @@ const makeHobbits = () => {
   console.log('Make hobbits');
 
   // 1. display an unordered list of the hobbits in the shire.
-
+  const $ulHobbits = $('<ul/>')
+  for (let i = 0; i < hobbits.length; i++){
+    $ulHobbits.append($('<li/>').attr('class','hobbit').text(hobbits[i]));
+  }
   // 2. give each hobbit a class of "hobbit"
 
   // hint: create a 'ul' outside the loop upon which to append the 'li's
 
   // hint: get 'The-Shire' by using its id
-
+  $('#The-Shire').append($ulHobbits);
 };
 
 // COMMIT YOUR WORK
